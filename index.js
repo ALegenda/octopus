22efb0f97918fbaa19274d140c8d1d0ebc82450c
TomKuper <adalekseev@edu.hse.ru>
2017-07-27 12:30:08 +1000

    Second commit

---
+++ index.js
@@ -0,0 +1,75 @@
+var express = require('express');
+var app = express();
+var qs = require('qs');
+var get = require('simple-get');
+var url = "mongodb://TomKuper:dbpassword1234@ds123933.mlab.com:23933/heroku_51tvcl2l";
+var mongodb = require("mongodb");
+var db;
+var ObjectID = require('mongodb').ObjectID;
+
+app.set('port', (process.env.PORT || 5000));
+app.set('views', __dirname);
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+
+mongodb.MongoClient.connect(
+    process.env.MONGODB_URI || url,
+    function (err, database)
+    {
+        if (err)
+        {
+            console.log(err);
+        }
+
+        db = database;
+        console.log('ok');
+
+    }
+);
+
+app.get(
+    '/admin',
+    function (request, response)
+    {
+        response.render("admin.html");
+    }
+);
+
+app.get(
+    '/',
+    function (request, response)
+    {
+        response.render("index.html");
+    }
+);
+
+app.get(
+    '/kek',
+    function (request, response)
+    {
+        response.send("kek");
+    }
+);
+
+app.get(
+    '/api/:compet',
+    function (request, response)
+    {
+        var token = request.params.compet;
+        var collection = db.collection(compet);
+        collection.find({}).toArray(
+            function (err, results)
+            {
+                response.send(results); // output all records
+            }
+        );
+    }
+);
+
+app.listen(
+    app.get('port'),
+    function ()
+    {
+        console.log('Node app is running on port', app.get('port'));
+    }
+);
